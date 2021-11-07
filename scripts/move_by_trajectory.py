11644152d256932d5181a63b3dec7bdf2d8043d9
vas0x59 <vas0706@mail.ru>
2021-11-07 13:51:12 +0300

    SetModelState

---
+++ scripts/move_by_trajectory.py
@@ -0,0 +1,73 @@
+import numpy as np
+import rospy
+from gazebo_msgs.srv import SetModelState
+from gazebo_msgs.msg import ModelState
+from geometry_msgs.msg import Pose, Twist, Point, Vector3, Quaternion
+import tf.transformations as tf_t
+
+rospy.init_node("move")
+# rospy.wait_for_service('add_two_ints')
+set_model_state = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
+
+poses = [
+    [np.array([0, 0, 0]), np.array([0, 0, 0])],
+    [np.array([0, 1, 1]), np.array([0, 0, -0.9])],
+    [np.array([0, 1, 0]), np.array([0, 0, 0])]
+]
+poses.append(poses[-1])
+hz = 0.5
+
+# prev = [np.array([0, 0, 0]), np.array([0, 0, 0])]
+
+
+
+
+def orientation_from_quaternion(q):
+    return Quaternion(*q)
+
+
+def orientation_from_euler(r, p, y):
+    q = tf_t.quaternion_from_euler(r, p, y)
+    return orientation_from_quaternion(q)
+
+
+for i in range(len(poses)-1):
+    now = poses[i]
+    nxt = poses[i+1]
+
+    v = (nxt[0] - now[0]) / (1/hz)
+    w = (nxt[1] - now[1]) / (1/hz)
+    print(v, w)
+
+    # prev = [np.copy(pnt), np.copy(rot)]
+    set_model_state(model_state=ModelState(
+        model_name="test",
+        pose=Pose(
+            position=Point(
+                x=now[0][0],
+                y=now[0][1],
+                z=now[0][2]
+            ),
+            orientation=orientation_from_euler(
+                r=now[1][0],
+                p=now[1][1],
+                y=now[1][2]
+            )
+        ),
+        twist=Twist(
+            linear=Vector3(
+                x=v[0],
+                y=v[1],
+                z=v[2]
+            ),
+            angular=Vector3(
+                x=w[0],
+                y=w[1],
+                z=w[2]
+            )
+        ),
+        reference_frame="world"
+    ))
+    rospy.sleep(1/hz)
+
+
